4c701cbb1c746d0d7cb5900e7554b549b3438651
ronaldfelix <arefqf@gmail.com>
2025-06-15 18:15:14 -0500

    fix: update backend connection and add cloudflare setup instructions

--- frontend/src/utils/conexion.ts
+++ frontend/src/utils/conexion.ts
@@ -1,3 +1,3 @@
-//export const BACKEND = 'http://localhost:8000';
-export const BACKEND = 'http://192.168.1.200:8000';
-export const API_BASE_URL = `${BACKEND}/api`;
+export const BACKEND = 'http://localhost:8000';
+//export const BACKEND = 'https://technologies-pet-wax-convergence.trycloudflare.com';
+//export const API_BASE_URL = `${BACKEND}/api`;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import{BACKEND} from './src/utils/conexion'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    allowedHosts: ['.ngrok-free.app'],
     proxy: {
       '/api': {
         //target: 'http://localhost:8000',
-        target: 'http://192.168.1.200:8000',
+        //target: 'http://192.168.1.200:8000',
+        target: BACKEND,
         changeOrigin: true,
         secure: false
       }
